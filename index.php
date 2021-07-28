63a92128bd263337d31456b0cf3b70eee8e9b6e0
minjun lee <84955607+devkr-potato@users.noreply.github.com>
2021-07-28 16:28:03 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>main</title>
+</head>
+<body>
+    <div class="nav">
+        <a class="name">name</a>
+        <div class="nav-right">
+            <a href="./login.php">로그인</a>
+        </div>
+    </div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login - test</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <form action="./index.php">
+        <input type="email" placeholder="Emaill">
+        <input type="password" placeholder="Password">
+        <input type="submit" value="로그인" class="btn-login">
+        <a href="./signin.php" class="btn-signin">회원가입</a>
+    </form>
+</body>
+<script src="./script.js"></script> 
+</html>
